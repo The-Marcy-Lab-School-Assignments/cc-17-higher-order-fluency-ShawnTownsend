208c34cb43732ffd7fb93b75138c3097d63ff674
ShawnTownsend <stitans28@gmail.com>
2021-12-07 15:38:19 +0000

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//write your code here
+function pluralize(array) {
+  return array.map((el) => `${el}s`);
+}
+
+function uppercase(array) {
+  return array.map((a) => a.charAt(0).toUpperCase() + a.substr(1));
+}
+
+function longWords(array) {
+  return array.filter((longWords) => longWords.length > 3);
+}
+
+function shortWords(array) {
+  return array.filter((longWords) => longWords.length < 5);
+}
+
+function oddLength(array) {
+  return array.filter((word) => word.length % 2);
+}
+
+function longToShort(array) {
+  return array.sort((a, b) => b.length - a.length);
+}
+
+function sum(nums) {
+    return nums.reduce((a, b) => a + b, 0);
+}
